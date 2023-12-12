825a24676b8d7a32b9ceac11f0f9eeb534db76fb
Timur <tim.akhmet@gmail.com>
2023-12-13 01:12:47 +0500

    new features as search and total

--- obmnl-flask_assignment/app.py
+++ obmnl-flask_assignment/app.py
@@ -70,6 +70,22 @@ def delete_transaction(transaction_id):
     # Redirect to the transactions list page
     return redirect(url_for("get_transactions"))
 
+@app.route('/search', methods=['GET', 'POST'])
+def search_transactions():
+    if request.method == 'POST':
+        min_amount = float(request.form['min_amount'])
+        max_amount = float(request.form['max_amount'])
+
+        filtered_transactions = [
+            transaction for transaction in transactions
+            if min_amount <= transaction['amount'] <= max_amount
+        ]
+
+        return render_template('transactions.html', transactions=filtered_transactions)
+
+    # Render the search form template for GET requests
+    return render_template('search.html')
+
 # Run the Flask app
 if __name__ == "__main__":
     app.run(debug=True)
